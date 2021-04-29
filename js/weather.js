c83bd3718ae6a2af2e46d35ce4e53fbaffa2759f
RoySKwon <roykwonfirmen@gmail.com>
2021-04-29 21:03:37 +0200

    add Geo and weather Data

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,62 @@
-function init() {}
+const API_KEY = "c20c2e4f835c7ca9edf8329477a6d36a";
+
+const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?";
+
+const weather = document.querySelector(".js-weather .weather__text");
+
+function getWeather(coords) {
+  fetch(
+    `${WEATHER_API}lat=${coords.lat}&lon=${coords.lng}&appid=${API_KEY}&units=metric`
+  )
+    .then((Response) => Response.json())
+    .then((json) => {
+      const name = json.name;
+      const temperature = json.main.temp;
+      weather.innerHTML = `${Math.floor(temperature)}°C @ ${name}`;
+    });
+  return;
+}
+
+function handleGeoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+
+  const coords = {
+    lat,
+    lng,
+  };
+  //Set Geo Data to Local Storage
+  localStorage.setItem("coords", JSON.stringify(coords));
+  getWeather(coords);
+
+  return;
+}
+
+function handleGeoFailure() {
+  console.log("no location Data");
+  return;
+}
+
+function loadWeather() {
+  const currentCoords = localStorage.getItem("coords");
+
+  if (currentCoords !== null) {
+    const parsedCoords = JSON.parse(currentCoords);
+    getWeather(parsedCoords);
+  } else {
+    navigator.geolocation.getCurrentPosition(
+      handleGeoSuccess,
+      handleGeoFailure
+    );
+  }
+
+  return;
+}
+
+function init() {
+  loadWeather();
+
+  return;
+}
 
 init();
