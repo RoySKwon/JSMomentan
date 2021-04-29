fa155f71e55d15dd6854f0980cc98bb735d70663
RoySKwon <roykwonfirmen@gmail.com>
2021-04-29 11:18:21 +0200

    add Base Code with Clock

---
+++ js/clock.js
@@ -0,0 +1,27 @@
+const clock = document.querySelector(".js-clock .clock__text");
+
+function getTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const days = now.getDate();
+
+  //Not Yet : days, seconds
+  const time = `${hours < 10 ? `0${hours}` : hours} : ${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`;
+
+  console.log(time);
+  clock.innerHTML = time;
+  return;
+}
+
+function init() {
+  //   debugger;
+  getTime();
+  setInterval(getTime, 1000);
+  return;
+}
+
+init();
